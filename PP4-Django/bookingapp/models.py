d88effd493bd3f8e5909d6fe5ef79a9ebcc85f5b
Gallie83 <102595438+Gallie83@users.noreply.github.com>
2022-11-24 15:42:45 +0000

    Created Booking model with list of available slot times

--- PP4-Django/bookingapp/models.py
+++ PP4-Django/bookingapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from datetime import date
 
-# Create your models here.
+Slots = (
+        ('9.00 - 10.00', '9.00 - 10.00'),
+        ('10.00 - 11.00', '10.00 - 11.00'),
+        ('11.00 - 12.00', '11.00 - 12.00'),
+        ('12.00 - 13.00', '12.00 - 13.00'),
+        ('13.00 - 14.00', '13.00 - 14.00'),
+        ('14.00 - 15.00', '14.00 - 15.00'),
+        ('15.00 - 16.00', '15.00 - 16.00'),
+        ('16.00 - 17.00', '16.00 - 17.00'),
+        ('17.00 - 18.00', '17.00 - 18.00'),
+)
+
+
+class Booking(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    slot = models.CharField(max_length=15, choices=Slots)
+    booking_date = models.CharField(max_length=15, default='foo')
+
+    def __str__(self):
+        return f'{self.user} has booked a PT at {self.slot} on {self.booking_date}'
