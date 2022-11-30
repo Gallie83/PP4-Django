e6209aba47728f69cef52a73c3a0cc914ffe007b
Gallie83 <102595438+Gallie83@users.noreply.github.com>
2022-11-30 11:03:11 +0000

    Created login view in members app

--- PP4-Django/members/views.py
+++ PP4-Django/members/views.py
@@ -4,5 +4,12 @@ from django.contrib import messages
 
 
 def login_user(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
 
-    return render(request, 'authenticate/login.html', {})
+    else:
+        return render(request, 'authenticate/login.html', {})
