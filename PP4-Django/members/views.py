072dda4be4eeaf86e5a009f1451ab36c965f7066
Gallie83 <102595438+Gallie83@users.noreply.github.com>
2022-11-30 11:15:39 +0000

    Added login validation and error message

--- PP4-Django/members/views.py
+++ PP4-Django/members/views.py
@@ -8,8 +8,14 @@ def login_user(request):
         email = request.POST['email']
         password = request.POST['password']
         user = authenticate(request, email=email, password=password)
+        # If user logs in successfully, redirect them to home page
         if user is not None:
             login(request, user)
+            return redirect('home')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ("Error logging in, please try again."))
+            return redirect('login')
 
     else:
         return render(request, 'authenticate/login.html', {})
