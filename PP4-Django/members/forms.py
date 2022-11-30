06c24cb4b69bfc5d05798476fa25f6cb48d44a08
Gallie83 <102595438+Gallie83@users.noreply.github.com>
2022-11-30 15:24:33 +0000

    Booking page only available if logged in, footer styling fixed

--- PP4-Django/members/forms.py
+++ PP4-Django/members/forms.py
@@ -4,8 +4,16 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
