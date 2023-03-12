c333b9891f5b40f5c3045a1a0e2841d337f2b078
Gallie83 <keving2913@hotmail.com>
2023-03-12 12:01:25 +0000

    Edit booking functionality added

--- bookingapp/urls.py
+++ bookingapp/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
-from .views import home, DateView, BookingView, confirmed, view_bookings, delete_bookings
+from .views import home, DateView, BookingView, confirmed, edit_date, edit_booking, edit_confirmed, view_bookings, delete_bookings
 
 urlpatterns = [
     path('', home, name="home"),
     path('date/', DateView, name="date"),
     path('book/', BookingView, name="book"),
+    path('edit_booking/', edit_booking, name="edit_booking"),
     path('confirmed/', confirmed, name="confirmed"),
+    path('edit_confirmed/', edit_confirmed, name="edit_confirmed"),
     path('view_bookings/', view_bookings, name="view_bookings"),
+    path('edit_date/<booking_id>',
+         edit_date, name='edit_date'),
+    path('edit_date/<booking_id>',
+         edit_booking, name='edit_booking'),
     path('delete_bookings/<booking_id>',
          delete_bookings, name='delete_bookings'),
 ]
--- bookingapp/views.py
+++ bookingapp/views.py
@@ -51,23 +51,15 @@ def view_bookings(request):
         return redirect('login')
 
 
-# Allows user to delete selected booking
-@login_required
-def delete_bookings(request, booking_id):
-    booking = Booking.objects.get(pk=booking_id)
-    booking.delete()
-    messages.success(request, ("Booking has been deleted!"))
-
-    return redirect('view_bookings')
-
-
 # Form that user chooses date for booking
 def DateView(request):
     form = DateForm(request.POST or None)
     if form.is_valid():
         # Takes input and converts from str to date
         formDate = form.cleaned_data.get('booking_date')
+        print(formDate)
         newDate = datetime.datetime.strptime(formDate, '%Y-%m-%d').date()
+        print(newDate)
         # Checks if date is before current date and throws error message
         if newDate < date.today():
             messages.success(request, ("Invalid date selected!"))
@@ -120,3 +112,95 @@ def BookingView(request):
         'available_slots': available_slots,
     }
     return render(request, "booking_form.html", context)
+
+
+def edit_date(request, booking_id):
+    old_booking = Booking.objects.get(pk=booking_id)
+    old_booking_pk = booking_id
+
+    form = DateForm(request.POST or None)
+    if form.is_valid():
+        # Takes input and converts from str to date
+        formDate = form.cleaned_data.get('booking_date')
+        newDate = datetime.datetime.strptime(formDate, '%Y-%m-%d').date()
+        # Checks if date is before current date and throws error message
+        if newDate < date.today():
+            messages.success(request, ("Invalid date selected!"))
+            return redirect('date')
+        # Stops user from making a booking on todays date
+        elif newDate == date.today():
+            messages.success(
+                request, ("Sessions must be booked at least a day in advance!"))
+            return redirect('date')
+        else:
+            # Stores inputted date as string to be used in booking form
+            request.session["dateSelected"] = str(
+                form.cleaned_data["booking_date"])
+            # Stores old booking data in session
+            request.session["oldBooking"] = str(old_booking)
+            request.session["oldBookingPk"] = str(old_booking_pk)
+
+            # Redirects user to booking form page
+            return redirect('edit_booking')
+
+    context = {
+        'old_booking': old_booking,
+        'old_booking_pk': old_booking_pk,
+        'form': form
+    }
+    return render(request, 'edit_date_form.html', context)
+
+
+def edit_booking(request):
+    # Retrieves date used in previous form and sets it to booking_date
+    instance = Booking(booking_date=request.session.get(
+        'dateSelected'), user=request.user)
+
+    # Gets previous booking data
+    old_booking = request.session.get("oldBooking")
+    print(old_booking)
+    old_booking_pk = request.session.get("oldBookingPk")
+
+    form = BookingForm(request.POST or None, instance=instance)
+
+    # Searches database for all sessions booked on chosen date
+    booked_slots = Booking.objects.filter(booking_date=request.session.get(
+        'dateSelected')).values_list('slot', flat=True)
+
+    available_slots = []
+
+    # Stores available times on chosen date in available_slots
+    for slot in Slots:
+        if slot[0] not in booked_slots:
+            available_slots.append(slot[0])
+
+    if form.is_valid():
+        form.save()
+        # Once new booking is made, remove old booking
+        previous_booking = Booking.objects.get(pk=old_booking_pk)
+        previous_booking.delete()
+        # Redirects user to confirmed booking page
+        return redirect('edit_confirmed')
+
+    context = {
+        'form': form,
+        'available_slots': available_slots,
+        'old_booking': old_booking,
+    }
+    return render(request, "edit_booking_form.html", context)
+
+# Renders page once user edits a session
+
+
+def edit_confirmed(request):
+    return render(request, 'edit_confirmed.html', {})
+
+
+# Allows user to delete selected booking
+@login_required
+def delete_bookings(request, booking_id):
+    booking = Booking.objects.get(pk=booking_id)
+    booking.delete()
+    messages.success(request, ("Booking has been deleted!"))
+
+    return redirect('view_bookings')
